0d52c8e0aee66767a7616085ce3e0ff1491ec4a9
Ramiro Ramirez <ramiro@kryha.io>
2022-06-13 10:32:47 +0200

    Add working version with emotion library

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import "@endo/init";
 // import 'ses';
 import logo from "./logo.svg";
 import "./App.css";
-import styled from "styled-components";
+// import styled from "styled-components";
+import styled from "@emotion/styled";
 
 const StyledDiv = styled.div`
   width: 100vw;
   height: 100vh;
   background: #333;
 `;
+const StyledP = styled.p`
+  color: white;
+`;
 
 function App() {
   return (
     <StyledDiv className="App">
       <header className="App-header">
+        <StyledP>This is my styled paragraph</StyledP>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </StyledDiv>
   );
